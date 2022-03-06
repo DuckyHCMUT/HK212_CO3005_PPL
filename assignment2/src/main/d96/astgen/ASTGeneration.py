ded6036f3074c2db0936a110825d41e5e1205826
Tran Quoc Viet <tqviet157@gmail.com>
2022-03-06 15:29:57 +0700

    Comment 3 import module lines
    Comment 3 import module lines before submitting to BKEL.

--- assignment2/src/main/d96/astgen/ASTGeneration.py
+++ assignment2/src/main/d96/astgen/ASTGeneration.py
@@ -6,9 +6,9 @@ from D96Parser import D96Parser
 from AST import *
 
 ### Remember to uncomment this mess before submit to BKEL ###
-from main.d96.utils.AST import ArrayCell, ArrayLiteral, Assign, AttributeDecl, BinaryOp, Block, BoolType, BooleanLiteral, Break, CallExpr, ClassDecl, ClassType, ConstDecl, Continue, FieldAccess, For, If, MethodDecl, NewExpr, NullLiteral, Return
-from main.d96.utils.AST import FloatLiteral, StringLiteral, IntLiteral, SelfLiteral, StringType, UnaryOp
-from main.d96.utils.AST import FloatType, Id, Instance, IntType, Program, Static, VarDecl, ArrayType, CallStmt
+# from main.d96.utils.AST import ArrayCell, ArrayLiteral, Assign, AttributeDecl, BinaryOp, Block, BoolType, BooleanLiteral, Break, CallExpr, ClassDecl, ClassType, ConstDecl, Continue, FieldAccess, For, If, MethodDecl, NewExpr, NullLiteral, Return
+# from main.d96.utils.AST import FloatLiteral, StringLiteral, IntLiteral, SelfLiteral, StringType, UnaryOp
+# from main.d96.utils.AST import FloatType, Id, Instance, IntType, Program, Static, VarDecl, ArrayType, CallStmt
 
 class ASTGeneration(D96Visitor):
     # Visit a parse tree produced by D96Parser#program.
